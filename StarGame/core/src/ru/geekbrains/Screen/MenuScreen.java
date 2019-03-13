2375c0abc0729b57366282dd8f12ff9d4825a594
AnnaGavrilova <gavrilovaa@mail.ru>
2019-03-13 23:51:59 +0500

    homework3

--- StarGame/core/src/ru/geekbrains/Screen/MenuScreen.java
+++ StarGame/core/src/ru/geekbrains/Screen/MenuScreen.java
@@ -3,64 +3,52 @@ package ru.geekbrains.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
+
 import ru.geekbrains.Base.Base2DScreen;
+import ru.geekbrains.Sprite.Background;
+import ru.geekbrains.Sprite.BadLogic;
 
 public class MenuScreen extends Base2DScreen {
-    private SpriteBatch batch;
-    private Texture img;
-    private Texture background;
-    private Vector2 touch;
-    private Vector2 pos;
-    private Vector2 vector; //вектор движения
-    private Vector2 v1; //скорость перемещения картинки
-    private int step = 5; //шаг - количество пикселей
-    private float count; // необходимое количество шагов
 
+    private Background background;
+    private Texture backImg;
+    private BadLogic badLogic;
+    private Texture img;
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
+        backImg = new Texture("starSky.jpg");
+        background = new Background(new TextureRegion(backImg));
         img = new Texture("badlogic.jpg");
-        background = new Texture("starSky.jpg");
-        touch = new Vector2();
-        pos = new Vector2(0, 0);
-        v1 = new Vector2();
-        vector = new Vector2();
+        badLogic = new BadLogic(new TextureRegion(img));
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0.51f, 0.34f, 0.64f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(background, 0, 0);
-        batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
+        badLogic.draw(batch);
         batch.end();
-        vector = touch.sub(pos);
-        count = vector.len()/step;
-        v1.set(vector.x/count, vector.y/count);
-        pos.add(v1);
-        if(pos.x == touch.x || pos.y == touch.y){
-            v1.setZero();
-        }
+        badLogic.update();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
         img.dispose();
-        background.dispose();
+        backImg.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+    public boolean touchDown(Vector2 touch, int pointer) {
+        badLogic.touchDown(touch, pointer);
         return false;
     }
 }
--- StarGame/core/src/ru/geekbrains/StarGame.java
+++ StarGame/core/src/ru/geekbrains/StarGame.java
@@ -7,6 +7,7 @@ import ru.geekbrains.Screen.MenuScreen;
 public class StarGame extends Game {
 	@Override
 	public void create() {
+
 		setScreen(new MenuScreen());
 	}
 }
